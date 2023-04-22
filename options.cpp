542e0d27cb01002eac98b4c9a3267b1d9548f6af
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 19:44:12 +0800

    Create options.cpp

---
+++ options.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "game.h"
+using namespace std;
+
+void options_1(int &IQ,int &pressure,int &parent_satisf){
+
+}
+
+void options_2(int &IQ,int &pressure,int &parent_satisf){
+    
+}
+
+void options_3(int &IQ,int &pressure,int &parent_satisf){
+    
+}
