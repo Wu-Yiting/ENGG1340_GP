59b766b851f63637e052f1de8c47a7c4f09643b7
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 11:33:41 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,7 +1,13 @@
 #ifndef GAME_H
 #define GAME_H
 
+bool game_end = false;
+int ending = -1;
+int game_stage = 1;
+
 void startgame();
-void options(int $IQ,int $pressure,int $parent_satisf);
+void options_1(int $IQ,int $pressure,int $parent_satisf);
+void options_2(int $IQ,int $pressure,int $parent_satisf);
+void options_3(int $IQ,int $pressure,int $parent_satisf);
 
 #endif
