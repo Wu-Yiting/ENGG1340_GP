c60101fd86e3ae19d1e6c2fa5ddd086640085619
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 01:15:24 +0800

    Update options.cpp

--- options.cpp
+++ options.cpp
@@ -11,15 +11,23 @@ void options_1(){
     cout << endl; 
     output("1.Study 2.Rest/Activities 3.Special events");
     cout << endl;
+    output("Your IQ:");
+    cout << IQ;
+    output("/200 Pressure: ");
+    cout << pressure;
+    output("/100 Parents Satisfication: ");
+    cout << parent_satisf;
+    output("/100");
+    cout << endl;
     int input_2;
     input_2 = inputting("123");
     if (input_2 == 1){
-        study();
+        study(game_stage);
     }else if (input_2 == 2){
-        rest();
+       rest();
     }
     else if (input_2 == 3){
-        special_events();
+       special_events();
     }
 }
 
