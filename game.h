77741f1653da4cb6f89c2ea0347031b19d3e0c2f
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 14:25:59 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,22 +1,23 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 #ifndef GAME_H
 #define GAME_H
 
-bool game_end = false;
-int ending = -1;
-int game_stage = 1;
-bool main_exam = false;
-int school_informs = -1;
-int IQ,pressure,parent_satisf;
+extern bool game_end;
+extern int ending;
+extern int game_stage;
+extern bool main_exam;
+extern int school_informs;
+extern int IQ,pressure,parent_satisf;
 
 void startgame();
-void options_1(int $IQ,int $pressure,int $parent_satisf);
-void options_2(int $IQ,int $pressure,int $parent_satisf);
-void options_3(int $IQ,int $pressure,int $parent_satisf);
-void exam_1(int $IQ,int $pressure,int $parent_satisf);
-void exam_2(int $IQ,int $pressure,int $parent_satisf);
-void exam_3(int $IQ,int $pressure,int $parent_satisf);
-void game_ending(int $ending);
+void options_1(int &IQ,int &pressure,int &parent_satisf);
+void options_2(int &IQ,int &pressure,int &parent_satisf);
+void options_3(int &IQ,int &pressure,int &parent_satisf);
+void exam_1(int &IQ,int &pressure,int &parent_satisf);
+void exam_2(int &IQ,int &pressure,int &parent_satisf);
+void exam_3(int &IQ,int &pressure,int &parent_satisf);
+void game_ending(int ending);
 
 #endif
