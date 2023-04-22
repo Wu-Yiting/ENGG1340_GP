16ce1968e5eff299879846cde5c30c544c57ed7b
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 00:03:40 +0800

    Update options.cpp

--- options.cpp
+++ options.cpp
@@ -2,14 +2,31 @@
 #include "game.h"
 using namespace std;
 
-void options_1(int &IQ,int &pressure,int &parent_satisf){
-
+void options_1(){
+    output("Your age: ");
+    cout << age;
+    output(" Month: ");
+    cout << month << endl;
+    output("Options:");
+    cout << endl; 
+    output("1.Study 2.Rest/Activities 3.Special events");
+    cout << endl;
+    int input_2;
+    input_2 = inputting("123");
+    if (input_2 == 1){
+        study();
+    }else if (input_2 == 2){
+        rest();
+    }
+    else if (input_2 == 3){
+        special_events();
+    }
 }
 
-void options_2(int &IQ,int &pressure,int &parent_satisf){
+void options_2(){
     
 }
 
-void options_3(int &IQ,int &pressure,int &parent_satisf){
+void options_3(){
     
 }
