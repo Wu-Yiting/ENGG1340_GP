15837b58218060a57e38053163a9090dfea263e8
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 00:03:54 +0800

    Update tool.cpp

--- tool.cpp
+++ tool.cpp
@@ -18,18 +18,22 @@ int change_input(string input){
     return input[input.length()-1] - '0';
 }
 
-
 void output(string s){
     cout << std::unitbuf;
-    stringstream ss(s);
-    string word;
-    while (ss>>word){
-        for (char c : word){
-        cout << c;
+    for (int i = 0;i<s.length();i++){
+        cout << s[i];
         std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
         }
-        cout << " ";
-    }
-    cout << endl;
+}
 
+int inputting(string ss){
+    string input_n;
+    while (true){
+        cin >> input_n;
+        if (valid(input_n,ss)){
+            return change_input(input_n);
+        }
+        cout << "Invalid input, please try angain" << endl;
+    }
+    return 0;
 }
