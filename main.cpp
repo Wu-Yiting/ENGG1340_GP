2e82f015ae725fabca91917ec10be74672375de5
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 11:33:33 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,22 @@
 #include "game.h"
 using namespace std;
 
-int main()
-{
+int main(){
     int IQ,pressure,parent_satisf;
+
     startgame();
-    options(IQ,pressure,parent_satisf);
+    while (not game_end){
+        if (game_stage == 1){
+            options_1(IQ,pressure,parent_satisf);
+        }
+        else if (game_stage == 2){
+            options_2(IQ,pressure,parent_satisf);
+        }
+        else if (game_stage == 3){
+            options_3(IQ,pressure,parent_satisf);
+        }
+        if (game_end) break;
+
+    }
     
 }
