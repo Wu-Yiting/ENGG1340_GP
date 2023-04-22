56830f28b7a453057b4a044e17e032b4d2c874ef
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 11:41:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,18 @@ int main(){
             options_3(IQ,pressure,parent_satisf);
         }
         if (game_end) break;
-
+        if (main_exam){
+            if (game_stage == 1){
+                exam_1(IQ,pressure,parent_satisf);
+            }
+            else if (game_stage == 2){
+                exam_2(IQ,pressure,parent_satisf);
+            }
+            else if (game_stage == 3){
+                exam_3(IQ,pressure,parent_satisf);
+            }
+        }
+           
     }
-    
+    game_ending(ending);
 }
