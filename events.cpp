268c4a7b8aa9cf2235a3ccbc2baddf341494062c
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 01:15:30 +0800

    Update events.cpp

--- events.cpp
+++ events.cpp
@@ -2,6 +2,31 @@
 #include "game.h"
 using namespace std;
 
-void study(){
-    
+void study(int stage){
+    int a,b,c;
+    a = IQ;b = pressure;c = parent_satisf;
+    IQ = IQ + 2*stage;
+    parent_satisf = parent_satisf + 10;
+    pressure = pressure + 4*stage;
+    if (IQ > 200){
+        IQ = 200;
+    }
+    if (pressure >= 100){
+        game_end = true;
+        ending = 1;
+        pressure = 100;
+        return;
+    }
+    if (parent_satisf > 100){
+        parent_satisf = 100;
+    }
+    cout << "Your IQ+" << IQ - a << endl;
+    Delay(500);
+    cout << "Your Pressure+"<< pressure - b << endl;
+    Delay(500);
+    cout << "The Parent Satisfication+" << parent_satisf - c << endl;
+    Delay(500);
+    cout << "==========================" << endl;
+    cout << endl;
+
 }
