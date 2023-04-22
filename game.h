a12d224b779e51dcb5b8f1711eab8807f8fa045c
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 13:32:13 +0800

    Update game.h

--- game.h
+++ game.h
@@ -8,6 +8,7 @@ int ending = -1;
 int game_stage = 1;
 bool main_exam = false;
 int school_informs = -1;
+int IQ,pressure,parent_satisf;
 
 void startgame();
 void options_1(int $IQ,int $pressure,int $parent_satisf);
