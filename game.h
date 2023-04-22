18d103af2d4f7bb3fb6d30f2f33658b9f9a7bb27
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-22 11:41:17 +0800

    Update game.h

--- game.h
+++ game.h
@@ -4,10 +4,15 @@
 bool game_end = false;
 int ending = -1;
 int game_stage = 1;
+bool main_exam = false;
 
 void startgame();
 void options_1(int $IQ,int $pressure,int $parent_satisf);
 void options_2(int $IQ,int $pressure,int $parent_satisf);
 void options_3(int $IQ,int $pressure,int $parent_satisf);
+void exam_1(int $IQ,int $pressure,int $parent_satisf);
+void exam_2(int $IQ,int $pressure,int $parent_satisf);
+void exam_3(int $IQ,int $pressure,int $parent_satisf);
+void game_ending(int $ending);
 
 #endif
