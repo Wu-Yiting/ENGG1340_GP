b2b9c5295346b5f7df7826985cb0a219a441e0df
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 00:03:56 +0800

    Create trash.cpp

---
+++ trash.cpp
@@ -0,0 +1,13 @@
+/*void output(string s){
+    cout << std::unitbuf;
+    stringstream ss(s);
+    string word;
+    while (ss>>word){
+        for (char c : word){
+        cout << c;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
+        }
+        cout << " ";
+    }
+
+}*/
