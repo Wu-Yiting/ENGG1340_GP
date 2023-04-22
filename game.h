9adfd3619ae459510ea6070fbd6ba7512d273413
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-04-23 00:03:35 +0800

    Update game.h

--- game.h
+++ game.h
@@ -6,28 +6,32 @@
 #include <time.h>
 #include <ctime>
 #include <string>
-#include <sstream>
 #include <chrono>
 #include <thread>
 using namespace std;
 
 extern bool game_end;
-extern int ending;
+extern int ending,age,month;
 extern int game_stage;
 extern bool main_exam;
 extern int school_informs;
 extern int IQ,pressure,parent_satisf;
 
 void startgame();
-void options_1(int &IQ,int &pressure,int &parent_satisf);
-void options_2(int &IQ,int &pressure,int &parent_satisf);
-void options_3(int &IQ,int &pressure,int &parent_satisf);
-void exam_1(int &IQ,int &pressure,int &parent_satisf);
-void exam_2(int &IQ,int &pressure,int &parent_satisf);
-void exam_3(int &IQ,int &pressure,int &parent_satisf);
+void options_1();
+void options_2();
+void options_3();
+void exam_1();
+void exam_2();
+void exam_3();
+void study();
+void rest();
+void special_events();
 void game_ending(int ending);
 void output(string s);
+void rungame();
 int change_input(string input);
 bool valid(string input,string a);
+int inputting(string ss);
 
 #endif
