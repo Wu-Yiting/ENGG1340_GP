e0db92dbdd5907171003a188d19164e6f22d092a
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-02 20:36:34 +0800

    Update startgame.cpp

--- startgame.cpp
+++ startgame.cpp
@@ -9,8 +9,8 @@ void startgame(){
     output_slow("Welcome to the game 'Get into the university!");cout << endl;
     output_slow("In this game, you will act as a student who want to get into the university.");cout << endl;
     output_slow("Your lifetime will start from the primary school.");cout << endl;
-    output_slow("The game is turn-based, each turn you will have 3 choice (or more when you grow up)");cout << endl;
-    output_slow("which are 1.study 2.rest/activities 3.other events(may be positive or negative)");cout << endl;
+    output_slow("The game is turn-based, each turn you will have 3 choice (or more when you grow up).");cout << endl;
+    output_slow("which are 1.study 2.rest/activities 3.other events(may be positive or negative).");cout << endl;
     output_slow("Your have three proprties 1.Your IQ 2.Your pressure 3.Parents satisfication.");cout << endl;
     output_slow("IQ will determine the difficulty of the test in each stage.");cout << endl;
     output_slow("The pressure will affect the ouccrance probability of special events, if your pressure is full you will fail the game.");cout << endl;
