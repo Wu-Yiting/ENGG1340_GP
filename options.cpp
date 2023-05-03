6e67aaa8312203408edd31a8e3c544ce352d2387
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-03 19:07:50 +0800

    Update options.cpp

--- options.cpp
+++ options.cpp
@@ -34,7 +34,37 @@ void options_1(){
 }
 
 void options_2(){
-    
+    cout << "==========================" << endl;
+    cout << endl;
+    output("Your age: ");
+    cout << age;
+    output(" Month: ");
+    cout << month << endl;
+    output("Options:");
+    cout << endl; 
+    output("1.Study 2.Rest 3.Special events 4.Hobby");
+    cout << endl;
+    output("Your IQ:");
+    cout << IQ;
+    output("/200 Pressure: ");
+    cout << pressure;
+    output("/100 Parents Satisfication: ");
+    cout << parent_satisf;
+    output("/100");
+    cout << endl;
+    int input_2;
+    while (true){
+        input_2 = inputting("1234");
+        if (input_2 == 4){
+            if (not if_hobby){
+                cout << "You have not develop a hobby (You have chance to develop a hobby in 'special events')" << endl;
+            }else{
+                break;
+            }
+        }else{
+            break;
+        }
+    }
 }
 
 void options_3(){
