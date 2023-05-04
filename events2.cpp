2608aadf6a7389ec5bc203f9c81fd4ad6f930e88
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-04 21:35:03 +0800

    Update events2.cpp

--- events2.cpp
+++ events2.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 void special_events(){
-    
+    int ran;
+    if (gmae_stage != 1){
+        if (not if_hobby){
+            srand(time(nullptr));
+            ran = rand() % 4 + 1;
+            if (ran == 1){
+                output("It's time for you to develop a hobby!(hobby may become your profession in the future)");cout<<endl;
+                
+            }
+        }
+    }
 }
 
 void hobby(){
