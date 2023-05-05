5bfe9a6b1ecb538bb2267366dbf475afd9f6ffbd
Wtyz123 <131432832+Wtyz123@users.noreply.github.com>
2023-05-06 01:10:02 +0800

    Update gameend.cpp

--- gameend.cpp
+++ gameend.cpp
@@ -8,53 +8,196 @@ void game_ending(int ending){
     if (ending == 1){//pressure is full.你由于过大的压力，得了抑郁症，暂停了学业，....， ，人生不只有大学，.....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 1"); cout << endl;
+        output_slow("Dear friend,I understand that you have been suffering from depression due to accumulated stress."); cout << endl;
+        output_slow("While education is important, it is not the only pursuit in our lives. We all know that a healthy mind and body are crucial to our quality of life. "); cout << endl;
+        output_slow("Therefore, I urge you to take a break from your studies and give yourself a chance to regain balance in your life."); cout << endl;
+        output_slow("Remember, education is not everything in life. It is merely a tool to help us achieve our dreams and goals."); cout << endl;
+        output_slow("When you feel sad and anxious, please seek help bravely. You can talk to your family, friends, or professional psychologists, and they will do their best to help you through this difficult time."); cout << endl;
+        output_slow("When you feel overwhelmed, do not be afraid to take a temporary break. This does not mean that you are not good enough, but that you need to take care of yourself."); cout << endl;
+        output_slow("I believe that when you adjust your physical and mental state, you will face future challenges more bravely."); cout << endl;
+        output_slow("You are not alone, and we are all here to support you. When you need someone to listen to your thoughts or share your feelings, please feel free to contact us anytime. Remember, we all care about your happiness and health."); cout << endl;
+        output_slow("Finally, I want to tell you that you are a very outstanding person."); cout << endl;
+        output_slow("You have many strengths and talents, and your future is full of infinite possibilities."); cout << endl;
+        output_slow("I wish you a speedy recovery to health and happiness."); cout << endl;
     }
     if (ending == 2){//parent satisfacation is zero.你和父母闹了矛盾，离家出走 ....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 2"); cout << endl;
+        output_slow("Dear child,I have heard about your confusion and conflicts, and I would like to offer you some advice and encouragement."); cout << endl;
+        output_slow("First of all, please remember that everyone has their limitations, and no one can meet others' expectations perfectly. Even your parents cannot expect you to be perfect in everything, and you do not need to blame yourself excessively for this."); cout << endl;
+        output_slow("As parents, they want you to succeed, and this is natural. However, everyone's definition and expectations of success are different."); cout << endl;
+        output_slow("Sometimes, their expectations may exceed your ability range, or they may not match your inner values."); cout << endl;
+        output_slow("In this case, you need to learn to communicate with them, express your feelings and thoughts, and seek consensus."); cout << endl;
+        output_slow("Please remember that your value does not depend on whether you meet your parents' expectations."); cout << endl;
+        output_slow("You are a unique individual with unlimited potential and possibilities. When facing challenges, maintain a firm belief and trust in your abilities and value."); cout << endl;
+        output_slow("Finally, I hope you can understand that there are no perfect people or parents in life."); cout << endl;
+        output_slow("We are all striving to become better versions of ourselves, and family is the cornerstone of our growth."); cout << endl;
+        output_slow("Please cherish your relationship with your family and believe that they will understand and support your growth."); cout << endl;
+        output_slow("Take the first step bravely to solve the problem, and may you find inner peace and confidence soon."); cout << endl;
     }
     if (ending == 3){//get into the university with low pressure.你轻松进了大学，在大学里你的成绩也很优异，毕业后你成为了....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 3"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 4){//get into the university with high pressure.你进了大学，在大学里，面对别人的竞争，压力不减，由于你过分要求自己，身体健康出了问题...最后你认清了....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 4"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 5){//the primary school final exam is failed.
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 5"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 6){//the middle school final exam is failed.
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 6"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 7){//the high school final exam is failed.
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 7"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 8){//hobby: drawing end 1. 你以你画画的特长，顺利进入名列前茅的世界美院....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 8"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 9){//hobby: drawing end 2. 你最终没有考上大学，但是凭借你画画的本领.....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 9"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 10){//hobby: basketball end 1. 你因过人的篮球技术，被大学破格录取.....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 10"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 11){//hobby: basketball end 2. 你最终没有考上大学，但是凭借你篮球的本领最终成为了偶像练习生....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 11"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 12){//hobby: singing end 1. 你以唱歌特长.....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 12"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
     if (ending == 13){//hobby: singing game end 2. 你最终没有考上大学，但是凭借你.....
         output_slow("The game is end."); cout << endl;
         output_slow("Ending number: 13"); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
+        output_slow(""); cout << endl;
     }
 }
