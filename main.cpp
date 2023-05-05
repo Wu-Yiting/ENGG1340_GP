02b085217065a038689e147164dbfae91e0ef89f
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-06 01:57:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ bool main_exam = false;
 int school_informs = -1;
 int IQ=20,pressure=0,parent_satisf=100,unlucky = 0,task_r = 0,task_n = 0;
 int age=9,month=1,hobby_id=-1,money = 10,round1 = 0,erase = 0,time1 = 5,hobby_value = 0,mo = 20,pare = 5;
-int list_4[10] = {15,16,17,18,19,20,21,22,23,24};
+int list_4[10] = {14,15,16,17,18,19,20,21,22,23};
 int list_3[10] = {10,11,12,13,14,15,16,17,18,19};
 int list_2[10] = {6,7,8,9,10,11,12,13,14,15};
 int list_1[10] = {0,1,2,3,4,5,6,7,8,9};
