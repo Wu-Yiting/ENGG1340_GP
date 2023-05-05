e0e34a4016011e672c6e722e31a9b2e8431fda75
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-05 17:50:45 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int ending = -1;
 int game_stage = 1;
 bool main_exam = false;
 int school_informs = -1;
-int IQ=20,pressure=0,parent_satisf=100,unlucky = 0,task_r = 0;
+int IQ=20,pressure=0,parent_satisf=100,unlucky = 0,task_r = 0,task_n = 0;
 int age=9,month=1,hobby_id=-1,money = 10,round = 0,erase = 0,time1 = 5,hobby_value = 0,mo = 20,pare = 5;
 
 int main(){
