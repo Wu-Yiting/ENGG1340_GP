3b746a52822356fab8e6ea62d9625aa138fab315
xaviershi1 <131464481+Xaviershi123@users.noreply.github.com>
2023-05-05 17:10:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "game.h"
 using namespace std;
 
-bool game_end = false,if_hobby = false,bob = true;
+bool game_end = false,if_hobby = false,bob = true,taske = false;
 int ending = -1;
 int game_stage = 1;
 bool main_exam = false;
 int school_informs = -1;
-int IQ=20,pressure=0,parent_satisf=100,unlucky = 0;
+int IQ=20,pressure=0,parent_satisf=100,unlucky = 0,task_r = 0;
 int age=9,month=1,hobby_id=-1,money = 10,round = 0,erase = 0,time1 = 5,hobby_value = 0,mo = 20,pare = 5;
 
 int main(){
